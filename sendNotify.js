d5a4ce3cb9c253a4f4eb93d52b64755fb8208946
MkJz <39215692+sdmkjz@users.noreply.github.com>
2021-08-23 12:15:18 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -169,9 +169,9 @@ if (process.env.PUSH_PLUS_USER) {
  * @param author 作者仓库等信息  例：`本脚本免费使用 By：xxxx`
  * @returns {Promise<unknown>}
  */
-async function sendNotify(text, desp, params = {}, author = '\n\n仅供用于学习') {
+async function sendNotify(text, desp, params = {}, author = 'By MkJz') {
   //提供6种通知
-  desp += '\n\n仅供用于学习\n仓库地址：https://github.com/zero205/JD_tencent_scf';//增加作者信息，防止被贩卖等
+  desp += '\n\n仅供用于学习\n博客地址：https://blog.sdmkjz.cn';//增加作者信息，防止被贩卖等
   await Promise.all([
     serverNotify(text, desp), //微信server酱
     pushPlusNotify(text, desp) //pushplus(推送加)
